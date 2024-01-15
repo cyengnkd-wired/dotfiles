e075dc2621c1ee2db110e085d9e53fd586c37766
Lu <154997465+cyenglu@users.noreply.github.com>
2024-01-14 21:53:55 -0400

    Update sectools.sh

--- setup/sectools.sh
+++ setup/sectools.sh
@@ -14,7 +14,7 @@ install_snap_app() {
     local appname="$1"
     local snap_app="$2"
     echo -e "$red[+]$reset Installing $appname"
-    sudo snap install "$snap_app" -y 1>/dev/null 2>error_snap.log
+    sudo snap install "$snap_app" 1>/dev/null 2>error_snap.log
 }
 
 go_tool() {
@@ -25,7 +25,7 @@ go_tool() {
 }
 
 Tools() {
-    clear
+    echo
     sleep 1
     echo -e "$red[*]$reset Python settings $red[*]$reset"
     install_app "Python version 3" "python3"
@@ -38,6 +38,11 @@ Tools() {
     source "$HOME/.virtualenvs/sec/bin/activate"
     pip3 install -r requirements.txt 1>/dev/null 2>pip_error.log
     echo
+    echo -e "$red[*]$reset APT Tools $red[*]$reset"
+    install_app "jq" "jq"
+    install_app "Git" "git"
+    install_app "curl" "curl"
+    
     echo -e "$red[*]$reset Snap apps $red[*]$reset"
     install_snap_app "Amass" "amass"
     echo
@@ -54,7 +59,7 @@ Tools() {
         user_sha256=$(sha256sum "go${go_version}.linux-amd64.tar.gz" | awk '{print $1}')
         echo "[!] Enter the hash [!] > "
         read -r expected_sha256
-        echo -e "$user_sha256\n$expected_sha256"
+        echo -e "[*****]\n$user_sha256\n$expected_sha256\n[*****]"
         if [ "$user_sha256" == "$expected_sha256" ]; then
             echo "[!] Hash match [!]"
             echo "[+] Installing Go [+]"
@@ -74,7 +79,6 @@ Tools() {
         fi
     fi
 
-    clear
     sleep 1
     echo -e "$red[*]$reset Sectools $red[*]$reset"
     go_tool "Assetfinder" "github.com/tomnomnom/assetfinder"
@@ -88,7 +92,21 @@ Tools() {
     go_tool "Hakrevdns" "github.com/hakluke/hakrevdns"
     go_tool "Metabigor" "github.com/j3ssie/metabigor"
     go_tool "GF" "github.com/tomnomnom/gf"
+    go_tool "DNSX" "github.com/projectdiscovery/dnsx/cmd/dnsx"
+    go_tool "gau" "github.com/lc/gau/v2/cmd/gau"
     go_tool "Unfurl" "github.com/tomnomnom/unfurl"
+    go_tool "Goop" "github.com/deletescape/goop"
+    go_tool "Hakcheckrul" "github.com/hakluke/hakcheckurl"
+    go_tool "Meg" "github.com/tomnomnom/meg"
+    go_tool "Nuclei" "github.com/projectdiscovery/nuclei/v3/cmd/nuclei"
+    go_tool "sdlookup" "github.com/j3ssie/sdlookup"
+    go_tool "PureDNS" "github.com/d3mondev/puredns/v2"
+    go_tool "xurlfind3r" "github.com/hueristiq/xurlfind3r/cmd/xurlfind3r"
+    go_tool "subjs" "github.com/lc/subjs"
+    go_tool "qsreplace" "github.com/tomnomnom/qsreplace"
+    go_tool "JSubfinder" "github.com/ThreatUnkown/jsubfinder"
+    go_tool "getJS" "github.com/003random/getJS"
+    go_tool "Notify" "github.com/projectdiscovery/notify/cmd/notify"
 }
 
 Tools
